03b0e0e51fb13734034ce8d1a8a8aa6ec1a2de4d
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-06 10:17:20 -0300

    feat: deps

---
+++ deps.ts
@@ -0,0 +1,5 @@
+export {
+  Application,
+  Router,
+  RouterContext,
+} from "https://deno.land/x/oak@v5.3.1/mod.ts";
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import {
   Application,
   Router,
   RouterContext,
-} from "https://deno.land/x/oak@v5.3.1/mod.ts";
+} from "../deps.ts";
 
 const app = new Application();
 const router = new Router();
