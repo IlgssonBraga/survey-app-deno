34ab482d135c40a7202fcf49ab1369a94e47617a
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-09 12:13:14 -0300

    feat: render all surveys

--- src/app/controllers/SiteController.ts
+++ src/app/controllers/SiteController.ts
@@ -3,10 +3,10 @@ import { renderFileToString, RouterContext } from "../../../deps.ts";
 
 class SiteController {
   async surveys(ctx: RouterContext) {
-    // const surveys = await Survey.findAll();
+    const surveys = await Survey.findAll();
     ctx.response.body = await renderFileToString(
       `${Deno.realPathSync(Deno.cwd())}/src/app/views/surveys.ejs`,
-      {},
+      { surveys },
     );
   }
 
